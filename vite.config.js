34bd97357dbbee3c0053f8f2da616310db7a31e0
Weibin <weibinyy@gmail.com>
2026-09-19 22:14:29 -0500

    Initialize React app with Vite

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    allowedHosts: ['macbook2019.tail885f80.ts.net'],
+  },
+})
